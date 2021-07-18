96654949ef576603802d77d4dcce2286924e593c
Josep Marcello <jspmarcello@live.com>
2021-07-19 02:45:39 +0700

    fix(ComingSoon): made the coming soon prettier

--- src/views/ComingSoon.svelte
+++ src/views/ComingSoon.svelte
@@ -1,16 +1,19 @@
 <div>
-  <h1>40X: This page is coming soon o(￣┰￣*)ゞ</h1>
+  <h1>Welcome!</h1>
+  <h2>Unfortunately, this page is still in the making.</h2>
+  <h2>Please come back soon! ( *^-^)ρ(*╯^╰)</h2>
 </div>
 
 <style>
   div {
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     height: 100vh;
   }
 
-  h1 {
+  h2 {
     margin: 0;
   }
 </style>
