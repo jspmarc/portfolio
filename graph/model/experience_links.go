7c03d11daffb929835c48413326c6fe0bb52bf45
Josep Marcello <jspmarcello@live.com>
2022-05-21 20:30:31 +0700

    remove Go-related files

--- graph/model/experience_links.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type ExperienceLinks struct {
-	Certificate *string `bson:"certificate"`
-	GitRepo     *string `bson:"git_repo"`
-	Deployment  *string `bson:"deployment"`
-}
--- graph/model/skill_content_icon.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type SkillContentIcon struct {
-	Src     SkillContentIconSrc `bson:"src"`
-	ImgName string              `bson:"img_name"`
-}
