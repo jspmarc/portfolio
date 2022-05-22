b660a8f1a5b6c55dc599dc30f47f67843f6615fc
Josep Marcello <jspmarcello@live.com>
2022-05-22 09:35:57 +0700

    feat(mobile): comingsoon page

--- src/lib/ComingSoon.svelte
+++ src/lib/ComingSoon.svelte
@@ -1,19 +1,34 @@
 <div>
 	<h1>Welcome!</h1>
-	<h2>Unfortunately, this page is still in the making.</h2>
-	<h2>Please come back soon! ( *^-^)ρ(*╯^╰)</h2>
+	<h2>
+		Unfortunately, this page is still in the making.<br />Please come back soon!
+		<span>( *^-^)ρ(*╯^╰)</span>
+	</h2>
 </div>
 
-<style>
+<style lang="scss">
+	@import '../styles/mixin.scss';
+
 	div {
 		align-items: center;
 		display: flex;
 		flex-direction: column;
 		height: var(--min-content-height);
 		justify-content: center;
+		padding: 0 2rem;
+		text-align: center;
+
+		@include respond-to('xl') {
+			padding: 0;
+			text-align: left;
+		}
 	}
 
 	h2 {
 		margin: 0;
+	}
+
+	span {
+		font-size: 0.9rem;
 	}
 </style>
