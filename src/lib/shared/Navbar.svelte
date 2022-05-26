4c375b1cac6b2e1aa1e8f9bf6a93ebafbdba470b
Josep Marcello <jspmarcello@live.com>
2022-05-26 23:22:18 +0700

    fix(navbar): update how it works and add socials for mobile

--- src/lib/shared/Navbar.svelte
+++ src/lib/shared/Navbar.svelte
@@ -7,13 +7,18 @@ Inspiration:
 	import NavItems from '../data/NavItem.js';
 	import SocialMedias from '../data/SocialMedia';
 	import { page } from '$app/stores';
+
+	let navbarExpanded = false;
 </script>
 
 <nav>
 	<ul class="container">
 		{#each NavItems as { text, link, icon }}
 			<li class="nav-item">
-				<a href={link} class={$page.url.pathname === link ? 'active' : ''}>
+				<a
+					href={link}
+					class={'nav-item-inner-container ' + ($page.url.pathname === link && 'active')}
+				>
 					<i class="fas fa-{icon} fa-2x icon" />
 					<span class="text">
 						{text}
@@ -26,7 +31,7 @@ Inspiration:
 			<ul class="social-medias">
 				{#each SocialMedias as { text, link, icon, isBrand }}
 					<li class="social-item">
-						<a href={link} target="_blank">
+						<a href={link} target="_blank" class="nav-item-inner-container">
 							<i class="{isBrand ? 'fab' : 'fas'} fa-{icon} fa-2x icon" />
 							<span class="text">
 								{text}
@@ -35,8 +40,35 @@ Inspiration:
 					</li>
 				{/each}
 			</ul>
+			<button
+				class="nav-item-inner-container socials-button {navbarExpanded && 'active'}"
+				on:click={() => (navbarExpanded = !navbarExpanded)}
+			>
+				<i class="fa-solid fa-circle-user fa-2x icon" />
+				<span class="text">Socials</span>
+			</button>
 		</li>
 	</ul>
+
+	{#if navbarExpanded}
+		<ul class="mobile-social-medias">
+			{#each SocialMedias as { text, link, icon, isBrand }}
+				<li class="social-item">
+					<a
+						href={link}
+						target="_blank"
+						class="nav-item-inner-container"
+						on:click={() => (navbarExpanded = false)}
+					>
+						<span class="text">
+							{text}
+						</span>
+						<i class="{isBrand ? 'fab' : 'fas'} fa-{icon} fa-2x icon" />
+					</a>
+				</li>
+			{/each}
+		</ul>
+	{/if}
 </nav>
 
 <style lang="scss">
@@ -47,11 +79,11 @@ Inspiration:
 		--navbar-item-icon-size: calc(0.3 * var(--navbar-width));
 
 		background-color: var(--cyan);
+		bottom: 0;
 		margin: 0;
 		height: var(--navbar-width);
 		position: fixed;
 		transition: 0.3s;
-		transform: translateY(calc(100vh - var(--navbar-width)));
 		width: 100vw;
 
 		.container {
@@ -64,28 +96,43 @@ Inspiration:
 			padding: 0;
 		}
 
+		.mobile-social-medias {
+			background-color: var(--cyan);
+			border-radius: 0.5rem 0.5rem 0 0;
+			bottom: var(--navbar-width);
+			list-style: none;
+			margin: 0;
+			padding: 0 2rem;
+			position: fixed;
+			right: 0;
+			transition: var(--transition-speed);
+			width: var(--navbar-open-width);
+
+			.text {
+				display: block;
+				flex-grow: 1;
+				margin-right: 1rem;
+			}
+		}
+
 		.nav-item {
 			width: 100%;
 
 			&:last-child {
-				display: none;
+				display: block;
 			}
 		}
 
-		a {
+		.nav-item-inner-container {
 			align-items: center;
 			color: var(--white);
+			cursor: pointer;
 			display: flex;
 			justify-content: flex-start;
 			height: var(--navbar-width);
 			outline: none; // FIXME: cursed...
 			text-decoration: none;
 			width: 100%;
-
-			&:hover {
-				color: var(--blue);
-				transition: var(--transition-speed);
-			}
 		}
 
 		.icon {
@@ -94,8 +141,7 @@ Inspiration:
 		}
 
 		.social-medias {
-			list-style: none;
-			padding: 0;
+			display: none;
 		}
 
 		.text {
@@ -111,7 +157,6 @@ Inspiration:
 
 		@include respond-to('xl') {
 			height: 100vh;
-			transform: translateY(0);
 			width: var(--navbar-width);
 
 			.container {
@@ -119,18 +164,38 @@ Inspiration:
 				height: 100%;
 			}
 
+			.mobile-social-medias {
+				display: none;
+			}
+
 			.nav-item:last-child {
 				display: block;
 				margin-top: auto;
 			}
 
-			a {
+			.nav-item-inner-container {
 				width: var(--navbar-width);
+				&:hover {
+					color: var(--blue);
+					transition: var(--transition-speed);
+				}
 			}
 
 			.icon {
 				margin: 0 1.5rem;
 			}
+
+			.social-medias {
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				list-style: none;
+				padding: 0;
+			}
+
+      .socials-button {
+        display: none;
+      }
 
 			/* Handle hover */
 			&:hover {
@@ -138,10 +203,8 @@ Inspiration:
 				width: var(--navbar-open-width);
 				transition: var(--transition-speed);
 
-				a {
+				.nav-item-inner-container {
 					width: var(--navbar-open-width);
-
-					transition: var(--transition-speed);
 				}
 
 				.text {
